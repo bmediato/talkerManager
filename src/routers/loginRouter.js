d316e160e36c153ba3ec31e9dcd029e94208634d
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-06 18:02:17 -0300

    feat: adiciona requisito 3

--- src/routers/loginRouter.js
+++ src/routers/loginRouter.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const generateToken = require('../Utils/generateToken');
 
 const router = express.Router();
+router.use(express.json());
 
+router.post('/', (req, res) => {
+  const { email, password } = req.body;
+  if (![email, password].includes(undefined)) {
+    const token = generateToken();
+    return res.status(200).json({ token });
+  }
+});
 module.exports = router;
